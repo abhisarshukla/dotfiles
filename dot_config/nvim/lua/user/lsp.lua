70a8178a30ad97433b83a8b61b601cd5712fd57b
abhisarshukla <abhisarshukla645@gmail.com>
2022-09-28 19:34:05 +0530

    More LSP, autocommands and command to reload lua config

--- dot_config/nvim/lua/user/lsp.lua
+++ dot_config/nvim/lua/user/lsp.lua
@@ -8,10 +8,26 @@ lsp_zero.setup()
 
 local config = {
   -- enable virtual text
-  virtual_text = true,
-  --disable signas
+  virtual_text = {
+    prefix = "",
+    format = function (diagnostic)
+      local icons = {
+       [vim.diagnostic.severity.ERROR] =  "",
+       [vim.diagnostic.severity.WARN] =  "喝",
+       [vim.diagnostic.severity.INFO] =  "ﱤ",
+       [vim.diagnostic.severity.HINT] = "",
+      }
+      return string.format("%s %s", icons[diagnostic.severity], diagnostic.message)
+    end
+  },
+  --disable signs
   signs = false,
+  -- only underline if there is error
+  underline = {
+    severity = vim.diagnostic.severity.ERROR,
+  },
   severity_sort = true,
+  update_in_insert = true, -- update diagnostic in insert mode
 }
 
 vim.diagnostic.config(config)
